e700b434cb505ae9676d0577ce06bf5a0e46593c
Alex Buhai <alexandruabuhai@gmail.com>
2018-04-17 21:42:57 +0300

    Added the first structure of the project
    I uploaded through here.
    Commit message:
     The first structure added, but I DID NOT MANAGE to make it to work with
     MySql so maybe we have to change it. Also some methods are not
     implemented.

---
+++ bloodDonor/bloodDonor/PacientRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bloodDonor
+{
+    public class DonorRepository : Repository
+    {
+        private List<Donor> donorList;
+    }
+}
