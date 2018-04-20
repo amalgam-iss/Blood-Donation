b9d604b96fea42dc773696f6d22bd49407def06d
Alexandru Buhai <alexandruabuhai@gmail.com>
2018-04-20 11:17:38 +0300

    Renamed the old project
    Renamed the old project such that people don't get confused.

---
+++ old-class-diagram-project/bloodDonor/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bloodDonor
+{
+    public interface IUser
+    {
+        void ILogin();
+        void ILogout();
+        void IChangePassword();
+    }
+}
---
+++ old-class-diagram-project/bloodDonor/Plasma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bloodDonor
+{
+    public class Plasma : BloodComponent
+    {
+        public Thrombocytes HasThrombocytes
+        {
+            get => default(Thrombocytes);
+            set
+            {
+            }
+        }
+    }
+}
